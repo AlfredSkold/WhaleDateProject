aa17cd2ca145c4a1795e498a2453b45f8c53c8e7
Alfred <Alfred@LAPTOP-PAO3N1NM>
2017-12-20 11:53:12 +0100

    Igen

---
+++ WhaleDateProject/WhaleDateProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhaleDateProject.Startup))]
+namespace WhaleDateProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
