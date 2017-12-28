9fc759bb137202839b2bd37b3c4400bf7522f42b
Alfred <Alfred@LAPTOP-PAO3N1NM>
2017-12-28 19:09:46 +0100

    Redigering någolunda klar

--- WhaleDateProject/WhaleDateProject/Controllers/EditController.cs
+++ WhaleDateProject/WhaleDateProject/Controllers/EditController.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using Data.Models;
 
 namespace WhaleDateProject.Controllers
@@ -30,6 +31,36 @@ namespace WhaleDateProject.Controllers
 
 
 
+            return RedirectToAction("Index", "Profile", new { username = User.Identity.Name });
+        }
+
+        public ActionResult Info(string Age, string Type, string Gender, string InterestedIn)
+        {
+            var thisUser = db.Users.Single(c => c.UserName == User.Identity.Name);
+
+            thisUser.Age = Age;
+            thisUser.Type = Type;
+            thisUser.Gender = Gender;
+            thisUser.InterestedIn = InterestedIn;
+            //if (Private== true)
+            //{
+            //    thisUser.Private = true;
+            //} else
+            //{
+            //    thisUser.Private = false;
+            //}
+            
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Profile", new { username = User.Identity.Name });
+        }
+
+        public ActionResult Bio(string Bio)
+        {
+            var thisUser = db.Users.Single(c => c.UserName == User.Identity.Name);
+
+            thisUser.Bio = Bio;
+            db.SaveChanges();
             return RedirectToAction("Index", "Profile", new { username = User.Identity.Name });
         }
     }
