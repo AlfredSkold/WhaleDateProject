094adc0119d42de9cbce298b3246fd01635f9d7b
Alfred <Alfred@LAPTOP-PAO3N1NM>
2017-12-28 14:02:10 +0100

    Search

---
+++ WhaleDateProject/WhaleDateProject/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WhaleDateProject.Controllers
+{
+    public class SearchController : BaseController
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search(string Searchstring)
+        {
+            var FoundProfiles = db.Users.Where(c => c.Firstname.Contains(Searchstring) || c.Lastname.Contains(Searchstring)).ToList();
+            return View(FoundProfiles);
+        }
+    }
+}
