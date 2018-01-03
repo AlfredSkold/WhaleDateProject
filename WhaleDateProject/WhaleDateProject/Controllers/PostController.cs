b5e34030a71f15c21fd71307463fdf899303dbb4
Alfred <Alfred@LAPTOP-PAO3N1NM>
2018-01-03 09:27:47 +0100

    lite

--- WhaleDateProject/WhaleDateProject/Controllers/PostController.cs
+++ WhaleDateProject/WhaleDateProject/Controllers/PostController.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace WhaleDateProject.Controllers
 {
-    public class PostController : BaseController
+    public class PostController : ApiController
     {
-        // GET: Post
-        public ActionResult Index()
-        {
-            return View();
-        }
     }
 }
